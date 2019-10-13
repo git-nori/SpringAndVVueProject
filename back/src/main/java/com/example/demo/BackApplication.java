518111f97266c0f65442ed5537d4b0dd5f2ef19b
HironoriSato <maza.hs5360@gmail.com>
2019-10-13 13:44:33 +0900

    [add]jpaによる監査を有効にするようアノテーションを追加

--- back/src/main/java/com/example/demo/BackApplication.java
+++ back/src/main/java/com/example/demo/BackApplication.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @SpringBootApplicationは下記の2つを自動追加する
 // @EnableAutoConfiguration => クラスパス設定、他のBean、およびさまざまなプロパティ設定に基づいてBeanの追加を開始するようにSpring Bootに指示する
 // @Configuration => アプリケーションコンテキストのBean定義のソースとしてクラスにタグを付ける
 @SpringBootApplication
+@EnableJpaAuditing // JPA監査を有効にする
 public class BackApplication {
 
 	public static void main(String[] args) {
